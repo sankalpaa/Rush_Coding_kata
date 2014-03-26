20b90f95a0d69b06981b3aa1a13d9f89339fa2c8
sankalpa <sab@exilesoft.com>
2014-03-26 11:31:00 +0530

    7000, 11812, 13014 unit test

--- PrintNumber/PrintNumber/PrintNumbers.cs
+++ PrintNumber/PrintNumber/PrintNumbers.cs
@@ -18,6 +18,9 @@ namespace PrintNumber
 		[TestCase(100, "one hundred")]
 		[TestCase(110, "one hundred and ten")]
 		[TestCase(556, "five hundred and fifty six")]
+		[TestCase(7000, "seven thousand")]
+		[TestCase(11812, "eleven thousand eight hundred and twelve")]
+		[TestCase(13014, "thirteen thousand and fourteen")]
 		public void ToEnglishShouldReturnNumberInText(int number, string numberInText)
 		{
 			ToEnglish(number).Should().Be(numberInText);
@@ -25,6 +28,7 @@ namespace PrintNumber
 
 		private string ToEnglish(int number)
 		{
+			int value;
 			var numberTextBuilder = new StringBuilder();
 			var tenth = new Dictionary<int, string>
 				{
@@ -41,19 +45,33 @@ namespace PrintNumber
 					{5, "five"},
 					{10, "ten"},
 					{6, "six"},
+					{7, "seven"},
+					{11, "eleven"},
+					{8, "eight"},
+					{12, "twelve"},
+					{13, "thirteen"},
+					{14, "fourteen"},
 			    };
+			if (number >= 1000)
+			{
+				value = number / 1000;
+				numberTextBuilder.Append(numbers[value]);
+				numberTextBuilder.Append(" thousand");
+				number %= 1000;
+			}
+
 			if (number >= 100)
 			{
-				int hundredValue = number / 100;
+				value = number / 100;
 				numberTextBuilder.Append(numberTextBuilder.Length > 1 ? " " : "");
-				numberTextBuilder.Append(numbers[hundredValue]);
+				numberTextBuilder.Append(numbers[value]);
 				numberTextBuilder.Append(" hundred");
 				number %= 100;
 			}
 
 			if (number >= 20)
 			{
-				int value = number / 10;
+				value = number / 10;
 				numberTextBuilder.Append(numberTextBuilder.Length > 1 ? " and " : "");
 				numberTextBuilder.Append(tenth[value * 10]);
 				number %= 10;
