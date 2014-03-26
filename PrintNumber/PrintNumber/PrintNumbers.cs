b726ae4cb03bc1176aff417a5b8d4fdb7a39037c
sankalpa <sab@exilesoft.com>
2014-03-26 10:31:07 +0530

    20, 21, 24, 30, 35 test cases

--- PrintNumber/PrintNumber/PrintNumbers.cs
+++ PrintNumber/PrintNumber/PrintNumbers.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using NUnit.Framework;
 using System.Collections.Generic;
 
@@ -9,6 +10,11 @@ namespace PrintNumber
 		[TestCase(1, "one")]
 		[TestCase(2, "two")]
 		[TestCase(3, "three")]
+		[TestCase(20, "twenty")]
+		[TestCase(21, "twenty one")]
+		[TestCase(24, "twenty four")]
+		[TestCase(30, "thirty")]
+		[TestCase(35, "thirty five")]
 		public void ToEnglishShouldReturnNumberInText(int number, string numberInText)
 		{
 			ToEnglish(number).Should().Be(numberInText);
@@ -16,14 +22,34 @@ namespace PrintNumber
 
 		private string ToEnglish(int number)
 		{
+			var numberTextBuilder = new StringBuilder();
+			var tenth = new Dictionary<int, string>
+				{
+					{20,"twenty"},
+					{30,"thirty"},
+					{50, "fifty"},
+				};
 			var numbers = new Dictionary<int, string>
 			    {
 				    {1, "one"},
 				    {2, "two"},
 				    {3, "three"},
+					{4, "four"},
+					{5, "five"},
 			    };
+			if (number >= 20)
+			{
+				int value = number / 10;
+				numberTextBuilder.Append(tenth[value * 10]);
+				number %= 10;
+			}
+			if (number != 0)
+			{
+				numberTextBuilder.Append(numberTextBuilder.Length > 1 ? " " : "");
+				numberTextBuilder.Append(numbers[number]);
+			}
 
-			return numbers[number];
+			return numberTextBuilder.ToString();
 		}
 	}
 }
