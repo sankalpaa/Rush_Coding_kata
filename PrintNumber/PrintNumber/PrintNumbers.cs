7b7cf5a5dacd8f6b127b3ce70f304f4bf89955be
sankalpa <sab@exilesoft.com>
2014-03-26 10:17:41 +0530

    1,2 and 3 test cases

--- PrintNumber/PrintNumber/PrintNumbers.cs
+++ PrintNumber/PrintNumber/PrintNumbers.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PrintNumber
 {
-    public class PrintNumbers
-    {
-    }
+	public class PrintNumbers
+	{
+		[TestCase(1, "one")]
+		[TestCase(2, "two")]
+		[TestCase(3, "three")]
+		public void ToEnglishShouldReturnNumberInText(int number, string numberInText)
+		{
+			ToEnglish(number).Should().Be(numberInText);
+		}
+
+		private string ToEnglish(int number)
+		{
+			var numbers = new Dictionary<int, string>
+			    {
+				    {1, "one"},
+				    {2, "two"},
+				    {3, "three"},
+			    };
+
+			return numbers[number];
+		}
+	}
 }
